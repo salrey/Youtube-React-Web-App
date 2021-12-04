47810546d7f0737ca99a20d8b71488ee614f8583
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-03 19:14:42 -0500

    import Search.js, added search componenet to return

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import { Component } from 'react';
 import './Home.css';
+import Search from 'Search.js'
 
 class Home extends Component {
     render() {
         return (
             <div className="Home"> 
-
+            <Search/>
             </div>
         )
     }
