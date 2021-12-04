5bb4aa16dd81fc6b63bb5e0eff9129a890d5ad3d
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-03 20:23:41 -0500

    add constructor method with default state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,6 @@
 import { Component } from "react";
 
 class Search extends Component {
-    constructor(){
-        super()
-        this.state = {
-            userInput: '',
-            result: null,
-        }
-    }
-
     render(){
         return(
             null
