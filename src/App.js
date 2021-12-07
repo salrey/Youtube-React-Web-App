121c978fc88032a91390a8966f251abc7d76e54c
salrey <tekdehn@gmail.com>
2021-12-07 14:24:38 -0500

    Add Video Component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,35 @@ import { Routes, Route } from 'react-router-dom';
 import Nav from "./components/Nav"
 import Home from "./components/Home"
 import About from "./components/About"
+import Video from './components/Video';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+        currentID: ""
+    }
+  }
+
+  renderVideo = (props) => {
+    const { id } = props.match.params;
+    console.log(props)
+    return <Video currentID={id} />
+  }
+  
+  changeID = (event) => {
+    // console.log(event.target.name)
+    this.setState({currentID: event.target.name})
+}
 
   render() {
     return (
       <div className="App">
         <Nav />
         <Routes> 
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
+          <Route exact path="//*" element={<Home changeID={this.changeID} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/videos/:id" element={<Video currentID={this.state.currentID}/>} />
         </Routes>
       </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,12 @@
 import { Component } from 'react';
 import './Home.css';
 import Search from './Search'
-import CommentSection from './CommentSection';
-import YouTube from 'react-youtube';
 
 class Home extends Component {
     render() {
         return (
             <div className="Home">
-                <Search />
-                <CommentSection />
+                <Search changeID={this.props.changeID} />
             </div>
         )
     }
