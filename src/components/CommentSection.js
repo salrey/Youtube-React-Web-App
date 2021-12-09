e7c1e4b95428a9be0e60668839a9424d4957969e
salrey <tekdehn@gmail.com>
2021-12-09 17:08:14 -0500

    Update CSS & CommentSection component

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -31,30 +31,64 @@ class CommentSection extends Component {
             userComments: [...userComments, userInput],
             userInput: { name: "", comment: "" }
         })
+
+        //Restart npm start to test localStorage
+        const storedComments = localStorage.getItem(this.props.videoId)
+
+        if (storedComments) {
+            localStorage.setItem(this.props.videoId, localStorage.getItem(this.props.videoId) + ", " + `{"name": "${userInput.name}", "comment": "${userInput.comment}"}`)
+        } else {
+            localStorage.setItem(this.props.videoId, `{"name": "${userInput.name}", "comment": "${userInput.comment}"}`)
+        }
     }
 
-    render() {
-        const { userComments, userInput } = this.state;
+    handleDelete = (event) => {
+        event.target.parentNode.remove()
+        
+        console.log(event.target.name)
 
+        this.setState({
+            // userComments: 
+        })
+    }
+
+    componentDidMount() {
+        const storedComments = localStorage.getItem(this.props.videoId)
+
+        if (storedComments) {
+            this.setState({
+                userComments: JSON.parse(`[${storedComments}]`),
+            })
+        }
+    }
+    
+    render() {
+        const { userComments, userInput} = this.state;
+        
+        // console.log(userComments)
+        
         return (
             <div className="CommentSection">
                 <form onSubmit={this.handleSubmit} className="CommentForm">
                     <div>
-                        <label for='name'> Name</label>
+                        <label htmlFor='name'> Username</label>
                         <input
                             type="text"
                             name="name"
-                            placeholder="Name..."
+                            placeholder="Enter 10 characters max..."
                             value={userInput.name}
-                            onChange={this.handleChange} />
+                            onChange={this.handleChange}
+                            maxLength="10" 
+                            required />
                     </div>
                     <div>
-                        <label for='comment'>Comment</label>
+                        <label htmlFor='comment'>Comment</label>
                         <textarea
                             name="comment"
-                            placeholder="..."
+                            placeholder="Enter 100 characters max..."
                             value={userInput.comment}
-                            onChange={this.handleChange} />
+                            onChange={this.handleChange}
+                            maxLength="100" />
                     </div>
                     <div>
                         <input type="submit" value="Submit" className='CommentSubmit' />
@@ -64,7 +98,8 @@ class CommentSection extends Component {
                 {
                     userComments.map((eachComment, index) => {
                         return (
-                            <li key={index} className='VideoComments'>
+                            <li key={index} name={index} className='VideoComments'>
+                                <button onClick={this.handleDelete}>Delete</button>
                                 <b>{eachComment.name}</b>
                                 <p>{eachComment.comment}</p>
                             </li>
