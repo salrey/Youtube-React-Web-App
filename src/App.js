1c77d39e20bc64c496cf4fe00150997161e83335
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-09 22:45:18 -0500

    import LogIn component, add route for login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import LogIn  from './components/LogIn'
 
 import Nav from "./components/Nav"
 import Home from "./components/Home"
@@ -20,6 +21,7 @@ class App extends Component {
         <Nav />
         <Switch> 
           <Route exact path="/" component={Home} />
+          <Route path="/login" component={LogIn} />
           <Route path="/about" component={About} />
           {/* PASS params as props USING react-router-dom V5 */}
           <Route path="/videos/:id" render={this.renderVideo} />
