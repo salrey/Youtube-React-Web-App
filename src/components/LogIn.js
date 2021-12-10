5f15e2bf62f40378d058e52998a795150821eef2
Rohan-Sterling <rohansterling@pursuit.org>
2021-12-09 23:49:55 -0500

    add state for userName and password

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 class LogIn extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            userName: '',
+            password: '',
+            rePassword: ''
+        }
+    }
     render(){
         return(
-            null
+            <form>
+                <label htmlFor="userName">User Name: </label>
+                <input name="userName" type="text" />
+                <label htmlFor="password">Password: </label>
+                <input name="password" type="text" />
+            </form>
         )
     }
 }
