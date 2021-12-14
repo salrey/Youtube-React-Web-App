f020e3e151278e404c5acd2f34a88229dc6e6be0
salrey <tekdehn@gmail.com>
2021-12-13 23:56:31 -0500

    Bug fix: add back other routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Nav from "./components/Nav"
 import Home from "./components/Home"
 import About from "./components/About"
 import Video from './components/Video';
+import LogIn  from './components/LogIn'
+import SignUp  from './components/SignUp'
 
 class App extends Component {
 
@@ -23,6 +25,8 @@ class App extends Component {
           <Route path="/about" component={About} />
           {/* PASS params as props USING react-router-dom V5 */}
           <Route path="/videos/:id" render={this.renderVideo} />
+          <Route path="/login" component={LogIn} />
+          <Route path="/signup" component={SignUp} />
           {/* using react-router-dom V6 */}
           {/* <Route path="/videos/:id" element={<Video currentID={this.state.currentID}/>} /> */}
         </Switch>
